380799acc39375d5c69d74c5e2d98f6beff094de
Jurijs Belozjorovs <Juriy@btv.lv>
2022-02-03 17:59:02 +0200

    1-3 tasks done

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,47 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = arr[0];
+  let max = arr[arr.length - 1];
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
 
-  return { min:min, max:max, avg:avg };
+  const avg = Number((sum / arr.length).toFixed(2));
+
+  return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
+  const sum = arr.reduce((a, b) => a + b, 0);
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = 0;
 
-  // Ваш кода
-  // for ...
-  
-  return max
+  for (let i = 0; i < arrOfArr.length; i++) {
+    if (max < func(arrOfArr[i])) {
+      max = func(arrOfArr[i]);
+    }
+  }
+
+  return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+
+  return Math.abs(max - min);
 }
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  return (
+    arr1.length === arr2.length &&
+    arr1.every((num, index) => num === arr2[index])
+  );
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr
+    .filter((num) => num > 0)
+    .filter((num) => !(num % 3))
+    .map((num) => num * 10);
 
-  // Ваш код
-
-  return resultArr; // array
+  return resultArr;
 }
