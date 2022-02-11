c992703815108d374156cb7e07c068aef53fedec
Jurijs Belozjorovs <Juriy@btv.lv>
2022-02-11 12:49:29 +0200

    4 task is done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  return (this.subject = subjectName);
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+  return this.marks;
+};
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = marks;
+  } else {
+    this.marks = this.marks.concat(marks);
+  }
+  return this.marks;
+};
+
+Student.prototype.getAverage = function () {
+  const sum = this.marks.reduce((a, b) => a + b, 0);
+  const average = sum / this.marks.length;
+  return average;
+};
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  return (this.excluded = reason);
+};
